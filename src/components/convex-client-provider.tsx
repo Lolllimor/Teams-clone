fe8778ca96b93b633503526061ab8eb965ec91ae
Lolliiimor <rmorin@africaexchange.com>
2024-11-28 14:39:29 +0100

    fixes: convex fix for redeployment

--- src/components/convex-client-provider.tsx
+++ src/components/convex-client-provider.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { ConvexReactClient } from 'convex/react';
-import { ConvexAuthNextjsProvider } from '@convex-dev/auth/nextjs';
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
-    <ConvexAuthNextjsProvider client={convex}>
-      {children}
-    </ConvexAuthNextjsProvider>
+     <ConvexProvider client={convex}>{children}</ConvexProvider>
   );
 }
